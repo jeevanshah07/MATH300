c08894842f05642eb8177696bb280376f233bf7d
Jeevan Shah <jedimaster3284@gmail.com>
2025-10-10 20:33:24 -0400

    update reference sheet

--- math2001/Math2001/03_Parity_and_Divisibility/01_Parity.lean
+++ math2001/Math2001/03_Parity_and_Divisibility/01_Parity.lean
@@ -13,7 +13,9 @@ example : Odd (7 : ℤ) := by
 
 
 example : Odd (-3 : ℤ) := by
-  sorry
+  dsimp [Odd]
+  use -2
+  numbers
 
 example {n : ℤ} (hn : Odd n) : Odd (3 * n + 2) := by
   dsimp [Odd] at *
@@ -25,7 +27,14 @@ example {n : ℤ} (hn : Odd n) : Odd (3 * n + 2) := by
 
 
 example {n : ℤ} (hn : Odd n) : Odd (7 * n - 4) := by
-  sorry
+  dsimp [Odd] at *
+  obtain ⟨ k, hk ⟩ := hn
+  use 7 * k + 1
+  calc
+    7 * n - 4 = 7 * (2 * k + 1) - 4 := by rw[hk]
+    _ = 14 * k + 7 - 4 := by ring
+    _ = 14 * k + 3 := by ring
+    _ = 2 * (7 * k + 1) + 1 := by ring
 
 example {x y : ℤ} (hx : Odd x) (hy : Odd y) : Odd (x + y + 1) := by
   obtain ⟨a, ha⟩ := hx
@@ -37,13 +46,32 @@ example {x y : ℤ} (hx : Odd x) (hy : Odd y) : Odd (x + y + 1) := by
 
 
 example {x y : ℤ} (hx : Odd x) (hy : Odd y) : Odd (x * y + 2 * y) := by
-  sorry
+  dsimp[Odd] at *
+  obtain ⟨ a, ha ⟩ := hx
+  obtain ⟨ b, hb ⟩ := hy
+  use 2 * a * b + 3 * b + a + 1
+  calc
+    x * y + 2 * y
+      = (2 * a + 1) * (2 * b + 1) + 2 * (2 * b + 1) := by rw[ha, hb]
+    _ = 2 * (2 * a * b + 3 * b + a + 1) + 1 := by ring
 
 example {m : ℤ} (hm : Odd m) : Even (3 * m - 5) := by
-  sorry
+  dsimp[Even, Odd] at *
+  obtain ⟨ k, hk ⟩ := hm
+  use 3 * k - 1
+  calc
+    3 * m - 5 = 3 * ( 2 * k + 1) - 5 := by rw[hk]
+    _ = 2 * (3 * k -1) := by ring
+
 
 example {n : ℤ} (hn : Even n) : Odd (n ^ 2 + 2 * n - 5) := by
-  sorry
+  dsimp[Even, Odd] at *
+  obtain ⟨ k, hk ⟩ := hn
+  use 2 * k ^ 2 + 2 * k - 3
+  calc
+    n^2 + 2*n - 5
+      = (2 * k)^2 + 2 * (2 * k) - 5 := by rw[hk]
+    _ = 2 * (2 * k ^ 2 + 2 * k - 3) + 1 := by ring
 
 example (n : ℤ) : Even (n ^ 2 + n + 4) := by
   obtain hn | hn := Int.even_or_odd n
@@ -62,42 +90,120 @@ example (n : ℤ) : Even (n ^ 2 + n + 4) := by
 
 
 example : Odd (-9 : ℤ) := by
-  sorry
+  use -5; numbers
 
 example : Even (26 : ℤ) := by
-  sorry
+  use 13; numbers
 
 example {m n : ℤ} (hm : Odd m) (hn : Even n) : Odd (n + m) := by
-  sorry
+  dsimp[Even, Odd] at *
+  obtain ⟨ a, ha ⟩ := hm
+  obtain ⟨ b, hb ⟩ := hn
+  use a + b
+  calc
+    n + m = 2 * b + (2 * a + 1) := by rw[ha, hb]
+    _ = 2 * (a + b) + 1 := by ring
 
 example {p q : ℤ} (hp : Odd p) (hq : Even q) : Odd (p - q - 4) := by
-  sorry
+  dsimp[Even, Odd] at *
+  obtain ⟨ a, ha ⟩ := hp
+  obtain ⟨ b, hb ⟩ := hq
+  use a - b - 2
+  calc
+     p - q - 4
+      = (2 * a + 1) - (2 * b) - 4 := by rw[ha, hb]
+    _ = 2 * (a - b - 2) + 1 := by ring
 
 example {a b : ℤ} (ha : Even a) (hb : Odd b) : Even (3 * a + b - 3) := by
-  sorry
+  dsimp[Even, Odd] at *
+  obtain ⟨ x, hx ⟩ := ha
+  obtain ⟨ y, hy ⟩ := hb
+  use 3 * x + y - 1
+  calc
+    3 * a + b - 3
+      = 3 * (2 * x) + (2 * y + 1) - 3 := by rw[hx, hy]
+    _ = 2 * (3 * x + y - 1) := by ring
 
 example {r s : ℤ} (hr : Odd r) (hs : Odd s) : Even (3 * r - 5 * s) := by
-  sorry
+  dsimp[Even, Odd] at *
+  obtain ⟨ x, hx ⟩ := hr
+  obtain ⟨ y, hy ⟩ := hs
+  use 3 * x - 5 * y - 1
+  calc
+    3 * r - 5 * s
+      = 3 * (2 * x + 1) - 5 * (2 * y + 1) := by rw[hx, hy]
+    _ = 2 * (3 * x - 5 * y - 1) := by ring
 
 example {x : ℤ} (hx : Odd x) : Odd (x ^ 3) := by
-  sorry
+  dsimp[Odd] at *
+  obtain ⟨ k, hk ⟩ := hx
+  use 4 * k ^ 3 + 6 * k ^2 + 3 * k
+  calc
+    x ^ 3 = (2 * k + 1) ^ 3 := by rw[hk]
+    _ = 2 * (4 * k ^ 3 + 6 * k ^ 2 + 3 * k) + 1 := by ring
 
 example {n : ℤ} (hn : Odd n) : Even (n ^ 2 - 3 * n + 2) := by
-  sorry
+  dsimp[Even, Odd] at *
+  obtain ⟨ k, hk ⟩ := hn
+  use 2 * k ^ 2 - k
+  calc
+    n ^ 2 - 3 * n + 2
+      = (2 * k + 1)^2 - 3 * (2 * k + 1) + 2 := by rw[hk]
+    _ = 2 * (2 * k ^ 2 - k) := by ring
 
 example {a : ℤ} (ha : Odd a) : Odd (a ^ 2 + 2 * a - 4) := by
-  sorry
+  dsimp[Odd] at *
+  obtain ⟨ k, hk ⟩ := ha
+  use 2 * k ^ 2 + 4 * k - 1
+  calc
+    a ^ 2 + 2 * a - 4
+      = (2 * k + 1)^2 + 2 * (2 * k + 1) - 4 := by rw[hk]
+    _ = 2 * (2 * k^2 + 4 * k - 1) + 1 := by ring
 
 example {p : ℤ} (hp : Odd p) : Odd (p ^ 2 + 3 * p - 5) := by
-  sorry
+  dsimp[Odd] at *
+  obtain ⟨ k, hk ⟩ := hp
+  use 2 * k ^ 2 + 5 * k - 1
+  calc
+    p ^ 2 + 3 * p - 5
+      = (2 * k + 1)^2 + 3 * (2 * k + 1) - 5 := by rw[hk]
+    _ = 2 * (2 * k^2 + 5 * k - 1) + 1 := by ring
 
 example {x y : ℤ} (hx : Odd x) (hy : Odd y) : Odd (x * y) := by
-  sorry
+  dsimp[Odd] at *
+  obtain ⟨ a, ha ⟩ := hx
+  obtain ⟨ b, hb ⟩ := hy
+  use 2 * a * b + a + b
+  calc
+    x * y = (2 * a + 1) * (2 * b + 1) := by rw[ha, hb]
+    _ = 2 * (2 * a * b + a + b) + 1 := by ring
 
 example (n : ℤ) : Odd (3 * n ^ 2 + 3 * n - 1) := by
-  sorry
+  obtain ha | hb := Int.even_or_odd n
+  · obtain ⟨ k, hk ⟩ := ha
+    use 6 * k ^ 2 + 3 * k - 1
+    calc
+      3 * n ^ 2 + 3 * n - 1
+        = 3 * (2 * k)^2 + 3 * (2 * k) - 1 := by rw[hk]
+      _ = 2 * (6 * k ^ 2 + 3 * k - 1) + 1 := by ring
+  · obtain ⟨ k, hk ⟩ := hb
+    use 6 * k ^ 2 + 9 * k + 2
+    calc
+      3 * n ^ 2 + 3 * n - 1
+        = 3 * (2 * k + 1) ^ 2 + 3 * (2 * k + 1) - 1 := by rw[hk]
+      _ = 2 * (6 * k ^ 2 + 9 * k + 2) + 1 := by ring
 
 example (n : ℤ) : ∃ m ≥ n, Odd m := by
-  sorry
+  dsimp[Odd] at *
+  obtain ha | hb := Int.even_or_odd n
+  · obtain ⟨ k, hk ⟩ := ha
+    use 2 * k + 1
+    calc
+      n = 2 * k := by rw[hk]
+      _ ≤ 2 * k + 1 := by extra
+  · obtain ⟨ k, hk ⟩ := hb
+    use 2 * k + 1
+    calc
+      n = 2 * k + 1 := by rw[hk]
 example (a b c : ℤ) : Even (a - b) ∨ Even (a + c) ∨ Even (b - c) := by
   sorry
